c0243c0f81febb6902478a1c7d6635fcf51ae599
Eliza Brine <elizabrine@gmail.com>
2017-12-08 13:04:38 -0800

    When an empty cell is clicked, text turns to letter K

--- src/components/Board.js
+++ src/components/Board.js
@@ -9,16 +9,16 @@ class Board extends Component {
   }
   cellClick(event) {
     console.log('hi')
-    console.log(event.target.id);
+    this.createCell(event.target.id, 'K')
   }
-  createCell(num) {
-    return <Cell num={num} key={num} onClick={this.cellClick} />
+  createCell(num, value) {
+    return <Cell num={num} key={num} value={value} onClick={this.cellClick} />
   }
   render(){
     return(<div >
       <h1>Board</h1>
       <div className="board">
-      {[1,2,3,4,5,6,7,8,9].map((num) => this.createCell(num))}
+      {[1,2,3,4,5,6,7,8,9].map((num) => this.createCell(num, ' '))}
       </div>
       </div>)
   }
--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,10 +3,20 @@ import React, { Component } from 'react';
 class Cell extends Component {
   constructor() {
     super();
+    this.onClick = this.onClick.bind(this);
+    this.state = {
+      letter: ' ',
+    }
+  }
+  onClick() {
+    if (this.state.letter = " ") {
+      this.setState({ letter: "K" });
+    }
   }
   render() {
     return (
-      <div className="cell" id={this.props.num} onClick={this.props.onClick}>
+      <div className="cell" id={this.props.num} onClick={this.onClick}>
+      <h1>{this.state.letter}</h1>
       </div>
     );
   }
