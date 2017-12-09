2b67c069e9195e2cbfb56e2618fca3b4f96b0697
Eliza Brine <elizabrine@gmail.com>
2017-12-08 17:32:18 -0800

    Remove scorecard from game for now

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,32 +5,12 @@ import $ from 'jquery'
 
 
 class Game extends Component {
-  constructor() {
-    super();
-    // this.play = this.play.bind(this)
-    this.state = {
-      currentPlayer: "user",
-      currentBoard: "+++++++++",
-    }
-  }
-  //
-  // play(event) {
-  //   let board_param = this.state.currentBoard
-  //   $.ajax({
-  //     url:`https://eb-tic-tac-toe.herokuapp.com/?board=${board_param}`
-  //   }).done((response) => {
-  //     console.log(response)
-  //     // this.setState({currentBoard: response})
-  //     return response
-  //   })
-  // }
 
   render(){
     return(<div>
       <div className='page'>
         <div className='container-fluid top-padding'>
           <div className='col-md-2'>
-            <ScoreCard />
           </div>
           <div className='col-md-8'>
             <Board playFunction={this.play} />
