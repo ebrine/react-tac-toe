eb7dae62166d1af47fa77ef258d5c975b644037f
Eliza Brine <elizabrine@gmail.com>
2017-12-18 12:16:32 -0800

    Adds custom messages for tie game, computer winning, user winning

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,12 +2,19 @@ import React, {Component} from 'react'
 
 class Banner extends Component {
   render(){
+    let message = ''
+    if (this.props.winner === "Tie") {
+      message = "Tie Game!"
+    } else if (this.props.winner === "Computer") {
+      message = "Computer Wins!"
+    } else if (this.props.winner === "You") {
+      message = "You Win!"
+    }
     return(<div>
        <div className="banner" style={{display: this.props.winner ? 'block' : 'none' }}>
-       <p className='bannertext'>{this.props.winner} win(s)</p>
+       <p className='bannertext'>{message}</p>
        <div className='reset' onClick={this.props.resetBoard}>Play Again</div>
        </div>
-
       </div>)
   }
 };
